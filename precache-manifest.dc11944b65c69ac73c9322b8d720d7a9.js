f69997ba76ce9985a1d47a6edd189093b7651cc4
divya <divyakumar913@gmail.com>
2018-12-17 15:21:09 +0100

    Updates

---
+++ precache-manifest.dc11944b65c69ac73c9322b8d720d7a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "fc52a5ecb262303a0e69",
+    "url": "/vidly/static/css/main.2fd48b73.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9149d248b31d7ef4909f",
+    "url": "/vidly/static/js/1.9149d248.chunk.js"
+  },
+  {
+    "revision": "ab00d1fec4a6e13b7ab4",
+    "url": "/vidly/static/js/runtime~main.ab00d1fe.js"
+  },
+  {
+    "revision": "fc52a5ecb262303a0e69",
+    "url": "/vidly/static/js/main.fc52a5ec.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "9149d248b31d7ef4909f",
+    "url": "/vidly/static/css/1.67c04ddb.chunk.css"
+  },
+  {
+    "revision": "85ab5a833ea4a26d99efdbd6b6ec9533",
+    "url": "/vidly/index.html"
+  }
+];
